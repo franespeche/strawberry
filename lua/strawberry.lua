cf362ac9c352edafa4e50e1ebe135248b6aea232
Fran Espeche <frean@Frans-MBP.fibertel.com.ar>
2024-01-08 09:19:26 -0300

    debug

--- lua/strawberry.lua
+++ lua/strawberry.lua
@@ -129,8 +129,7 @@ function Strawberry:open()
 
   -- vim.api.nvim_buf_add_highlight(buf, 0, 'strawberryKey', 1, 1, -1)
   if (vim.fn.has("syntax")) then
-    vim.fn.matchadd('strawberryKey', "/\\v^\\s\\s(\\d|\\a|\\s)/")
-    -- vim.cmd.syntax([[match strawberryKey /\v^\s\s(\d|\a|\s)/ contained]])
+    vim.cmd.syntax([[match strawberryKey /\v(\d|\a|\s)/ contained]])
     vim.cmd.syntax([[match strawberryName /\v^\s\s(\d|\a|\s)\s+.+\s\s/ contains=strawberryKey]])
     vim.cmd([[hi def link strawberryKey String]])
     vim.cmd([[hi def link strawberryName Type]])
