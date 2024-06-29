355e11d0ae353e3ca3bb0819d0171462086f8151
Fran Espeche <frean@Frans-MBP.fibertel.com.ar>
2024-06-29 18:11:24 -0300

    modularize item

--- lua/strawberry.lua
+++ lua/strawberry.lua
@@ -1,39 +1,8 @@
--- Utils
+-- Imports
+local Item = require('Item')
 local utils = require('utils')
 
--- Item
-local Item = {num = nil, title = nil, value = nil, action = nil}
-
--- TODO: get this from an object
-function Item:create(num, value, title, action)
-    local obj = {
-        num = num,
-        value = value,
-        title = title,
-        action = action or utils.open_file
-    }
-    setmetatable(obj, {__index = Item})
-    return obj
-end
-
-function Item:execute() self.action(self.value, self.ctx) end
-
-function Item:get_line_content(max_title_length)
-    local spacer = "  "
-    local line = "  " .. tostring(self.num)
-
-    local value = self.format_value(self.value)
-
-    if (self.title and self.title ~= "") then
-        line = line .. spacer .. self.title
-    end
-
-    local compensate_spaces = string.rep(' ', max_title_length - #self.title)
-    line = line .. compensate_spaces .. spacer .. value
-    return line
-end
-
--- helpers
+-- Helpers
 local function set_highlights()
     if (vim.fn.has("syntax")) then
         vim.cmd([[syntax clear]])
